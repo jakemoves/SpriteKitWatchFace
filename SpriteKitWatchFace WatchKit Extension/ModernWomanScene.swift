4db7561400a5add1eece2d1b7382e2c34bebb87f
Jacob Niedzwiecki <jacob@jakemoves.com>
2018-10-17 01:06:08 -0600

    Got a basic countdown working

--- SpriteKitWatchFace WatchKit Extension/ModernWomanScene.swift
+++ SpriteKitWatchFace WatchKit Extension/ModernWomanScene.swift
@@ -7,20 +7,39 @@
 
 import UIKit
 import SpriteKit
+import Foundation
 
 @objc class ModernWomanScene: SKScene, SKSceneDelegate {
     var lblTime: SKLabelNode?
     var elapsedTime: Double?
+    var startingTime: Date?
+    var timerDuration: Double?
     
     @objc public func setup() -> Void {
         lblTime = childNode(withName: "lbl_time_1") as! SKLabelNode
         lblTime?.text = "0"
+        startingTime = Date(timeInterval: 0.0, since: Date())
         elapsedTime = 0.0
+        
+        timerDuration = 10 * 60
     }
     
     override func update(_ currentTime: TimeInterval) {
-        
         elapsedTime? += currentTime
-        lblTime?.text = NumberFormatter.localizedString(from: NSNumber(value:elapsedTime!), number: .decimal)
+        
+        guard let elapsedSeconds = elapsedTime else { return }
+        
+        let remaining = timerDuration! - timeInSeconds(time: elapsedSeconds)
+    
+        let secondsRemaining = NumberFormatter.localizedString(from: NSNumber(value:remaining), number: .decimal)
+        
+        lblTime?.text = secondsRemaining
+    }
+    
+    func timeInSeconds(time:Double) -> Double {
+        var t = time
+        t /= 10000000
+        t = floor(t)
+        return t
     }
 }
